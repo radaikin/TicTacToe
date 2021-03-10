a334a86e81a5f980fe1365298697651e31229f17
Andrii Radaikin <andrii.r@moonactive.com>
2021-03-10 03:06:47 +0200

    Added name var to player

--- Assets/Scripts/GameScene/Players/AbstartctPlayer.cs
+++ Assets/Scripts/GameScene/Players/AbstartctPlayer.cs
@@ -2,22 +2,19 @@
 
 public abstract class AbstractPlayer : MonoBehaviour, IPlayer
 {
+    private string m_Name;
     private PlayerSide m_PlayerSide;
-
-    public AbstractPlayer()
-    {
-
-    }
 
     public PlayerSide GetPlayerSide()
     {
         return m_PlayerSide;
     }
 
-    public void SetSide(PlayerSide playerSide)
-    {
-        this.m_PlayerSide = playerSide;
-    }
+    public void SetName(string name) => m_Name = name;
+
+    public string GetName() => m_Name;
+
+    public void SetSide(PlayerSide playerSide) => m_PlayerSide = playerSide;
 
     protected bool MyStep()
     {
